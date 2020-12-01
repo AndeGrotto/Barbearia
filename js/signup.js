7e3a2cec56427d4ad94db1f299d271d635eb7b15
Luan Tonin Galvan <ltoningalvan@gmail.com>
2020-12-01 15:48:34 -0300

    Feito seus vadio

--- js/signup.js
+++ js/signup.js
@@ -4,6 +4,7 @@
     "load",
     function () {
       var forms = document.getElementsByClassName("needs-validation");
+      document.getElementById("mensagem").innerHTML = ``;
       var validation = Array.prototype.filter.call(forms, function (form) {
         form.addEventListener(
           "submit",
@@ -18,10 +19,19 @@
               const telefone = document.getElementsByName("telefone")[0].value;
               const data = document.getElementsByName("data")[0].value;
 
-              localStorage.setItem(
-                "cadastro",
-                JSON.stringify({ nome, email, telefone, data })
-              );
+              if (email.includes("@upf.br")) {
+                localStorage.setItem(
+                  "cadastro",
+                  JSON.stringify({ nome, email, telefone, data })
+                );
+                alert("Cadastro realizado com sucesso");
+                this.submit();
+              } else {
+                document.getElementById("mensagem").innerHTML = `
+                <div class="alert alert-danger" role="alert">
+                  O e-mail inserido não pertence à UPF
+                </div>`;
+              }
             }
           },
           false
